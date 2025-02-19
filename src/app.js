70d29f8d57bcd5426dbeb99f2247e63064338a43
Kshitij-Tidke <ktidke005@gmail.com>
2025-02-19 21:33:00 +0530

    1

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://ai-front-pi.vercel.app'],
     methods: ['GET', 'POST', 'DELETE', 'POST'],
   })
 );
